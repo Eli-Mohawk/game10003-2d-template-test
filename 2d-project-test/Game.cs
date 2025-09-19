bfaabb357e6e237571c66d5ee7eb0f0250ebf66d
Eli-Mohawk <eli.poag@mohawkcollege.ca>
2025-09-19 16:37:06 -0400

    Added rectangle mouse

---
+++ 2d-project-test/Game.cs
@@ -0,0 +1,52 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("This is a 2d test");
+            Window.SetSize(800, 800);
+            Window.TargetFPS = 60;
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            // clear the screen
+            Window.ClearBackground(Color.Black);
+
+            // draw a rectangle
+            Draw.FillColor = Color.Blue;
+
+            Color newColor = new Color(255, 165, 0);
+
+            Draw.LineColor = newColor;
+            Draw.LineSize = 10.0f;
+
+            //Vector2 rectanglePosition = new Vector2(300, 200);
+            Vector2 rectangleSize = new Vector2(200, 100);
+
+
+
+
+            Draw.Rectangle(Input.GetMousePosition() - rectangleSize / 2, rectangleSize);
+        }
+    }
+
+}
